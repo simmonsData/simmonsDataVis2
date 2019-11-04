e90e5d5feb32026cba462fd466e68a62a3fef57a
jlu38 <joeylu38@gmail.com>
2019-11-03 21:12:47 -0500

    add survey portion of model

--- backend/models/students.model.js
+++ backend/models/students.model.js
@@ -12,42 +12,85 @@ const studentSchema = new Schema({
         //https://emailregex.com/
     },
     updated_at : Date,
-    created_at : Date
-    // survey: {
-    //     general: {
-    //         question1: {
-    //             type: Number,
-    //         },
-    //         question2: {
-    //             type: String,
-    //         },
-    //         question3: {
-    //             type: String,
-    //         }
-    //     },
-    //     college: {
-    //         question1: {
-    //             type: String,
-    //         },
-    //         question2: {
-    //             type: String,
-    //         },
-    //         question3: {
-    //             type: String,
-    //         }
-    //     },
-    //     outOfClassActivity: {
-    //         question1: {
-    //             type: Array,
-    //         },
-    //         question2: {
-    //             type: Number
-    //         }
-    //     },
-    //     majorSatisfaction: {
+    created_at : Date,
+    survey: {
+        general: {
+            question1: {type: Number},
+            question2: {type: String},
+            question3: {type: String}
+        },
+        college: {
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String,}
+        },
+        outOfClassActivity: {
+            question1: {type: Array},
+            question2: {type: Number}
+        },
+        majorSatisfaction: {
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String},
+            question4: {type: String},
+            question5: {type: String},
+            question6: {type: String},
+            question7: {type: String},
+            question8: {type: String}
+        },
+        disciplineBelonging: {
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String}
+        },
+        majorValuing: {
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String}
+        },
+        achievmentStrinving: {
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String},
+            question4: {type: String},
+            question5: {type: String}
+        },
+        peerInteraction: {
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String},
+            question4: {type: String}   
+        },
+        gritScale: {
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String},
+            question4: {type: String},
+            question5: {type: String},
+            question6: {type: String},
+            question7: {type: String},
+            question8: {type: String},
+        },
+        typesOfTopActivity: {
+            question1: {type: String}
+        },
+        engineeringOutcomes: {
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String},
+            question4: {type: String},
+            question5: {type: String},
+            question1: {type: String},
+            question2: {type: String},
+            question3: {type: String}
+        },
+        phoneNumber: {
+            type: Number,
+            unique: true,
+            trim: true
+        }
 
-    //     }
-    // }
+    }
 });
 
 studentSchema.pre('save', function(next) {
