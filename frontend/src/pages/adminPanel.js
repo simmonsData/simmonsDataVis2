38605f2b7192e2f75f0b49158f70e534d600ba3d
Ivan Zundel <ivan.zundel@ufl.edu>
2019-11-13 21:56:52 -0500

    created AdminPanel js

---
+++ frontend/src/pages/adminPanel.js
@@ -0,0 +1,21 @@
+import React, {
+  Component
+} from 'react'
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+
+const adminPanel = () => ( <
+  div >
+  <
+  Button primary > Primary < /Button> <
+  Button secondary > Secondary < /Button> < /
+  div >
+)
+
+export default adminPanel
