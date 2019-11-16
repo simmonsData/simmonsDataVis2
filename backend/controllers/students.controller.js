eed5cac23fb3ee810701811fec3541e7a5db471c
blaisebowman <blaiseabowman@ufl.edu>
2019-11-16 14:56:48 -0500

    Updated email validation

--- backend/controllers/students.controller.js
+++ backend/controllers/students.controller.js
@@ -102,67 +102,75 @@ exports.delete = (req, res) => {
 
 // Registers a new student - post request
 exports.register = (req, res) => {
-    const { errors, isValid } = validateRegisterInput(req.body);
+    const {errors, isValid} = validateRegisterInput(req.body);
     // Checks if email is entered with valid format
-    if(!isValid){
-        return res.status(400).json(errors);
-    }
-
-    const email = req.body.email;
-
-    // Each entry in database is stored into student variable
-    Student.find({}, (err, student) => {
-        let matchFound = false;
-        if(err){
-            return res.status(400).send(err);
+    if (!isValid) {
+        if(errors){
+            //return res.json(errors);
+            res.status(400).send(errors);
         }
-        // If student is not empty, tries to find a match between entered email and hashed email in database
-        else if(student.length){
-            student.forEach((currentStudent) => {
-                const isMatch = bcrypt.compareSync(email, currentStudent.email)
-                    if(isMatch){
+        else {
+            return res.json(errors);
+        }
+        console.log(errors);
+    } else {
+
+        const email = req.body.email;
+
+        // Each entry in database is stored into student variable
+        Student.find({}, (err, student) => {
+            let matchFound = false;
+            if (err) {
+                return res.status(400).send(err);
+            }
+            // If student is not empty, tries to find a match between entered email and hashed email in database
+            else if (student.length) {
+                student.forEach((currentStudent) => {
+                    const isMatch = bcrypt.compareSync(email, currentStudent.email)
+                    if (isMatch) {
                         matchFound = true;
                     }
-            })
-
-        }
-
-        // If there are no students in the database or no matching email addresses, will create new entry
-        if(!student.length || matchFound != true){
-
-            // Making new student 
-            console.log("Creating new student");
-            const newStudent = new Student({
-                email: req.body.email
-            });
-
-            // Hashing email before saving into database
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newStudent.email, salt, (err, hash) => {
-                    if(err){
-                        throw err;
-                    }
-                    newStudent.email = hash;
-
-                    // Saving hashed email into database
-                    newStudent.save( (err) => {
-                        if(err) {
-                            console.log(err);
-                            return res.status(400).send(err);
-                        } 
-                        else {
-                            console.log(newStudent.email);
-                            return res.json(newStudent.email);
-                        }
-                    });      
                 })
-            })
-        }
-        // If matching email is found, returns "Email already created"
-        else{
-            return res.status(200).json({emailFound: "Email already created"}); 
-        }
-    });
+
+            }
+
+            // If there are no students in the database or no matching email addresses, will create new entry
+            if (!student.length || matchFound != true) {
+
+                // Making new student
+                console.log("Creating new student");
+                const newStudent = new Student({
+                    email: req.body.email
+                });
+
+                // Hashing email before saving into database
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newStudent.email, salt, (err, hash) => {
+                        if (err) {
+                            throw err;
+                        }
+                        newStudent.email = hash;
+
+                        // Saving hashed email into database
+                        newStudent.save((err) => {
+                            if (err) {
+                                console.log(err);
+                                return res.status(400).send(err);
+                            } else {
+                                console.log(newStudent.email);
+                                return res.json(newStudent.email);
+                            }
+                        });
+                    })
+                })
+            }
+            // If matching email is found, returns "Email already created"
+            else {
+                return res.status(200).json({emailFound: "Email already created"});
+            }
+
+        });
+    }
 }
 
 // Logins student by email address - post request
@@ -171,8 +179,13 @@ exports.login = (req, res) => {
 
     // Checks if email is valid
     if(!isValid){
-        return res.status(400).json(errors);
+        if(errors){
+            //return res.json(errors);
+            res.status(400).send(errors);
+        }
+        console.log(errors);
     }
+    else {
 
     const email = req.body.email;
     let hashedEmail;
@@ -180,14 +193,14 @@ exports.login = (req, res) => {
     // Each entry in database is stored into student variable
     Student.find({}, (err, student) => {
         let matchFound = false;
-        if(err){
+        if (err) {
             res.status(400).send(err);
         }
         // If student is not empty, tries to find a match between entered email and hashed email in database
-        else if(student.length){
+        else if (student.length) {
             student.forEach((currentStudent) => {
                 const isMatch = bcrypt.compareSync(email, currentStudent.email);
-                if(isMatch){
+                if (isMatch) {
                     matchFound = true;
                     hashedEmail = currentStudent.email;
                 }
@@ -195,15 +208,16 @@ exports.login = (req, res) => {
         }
 
         // If match is found in the database, returns hashed email
-        if(matchFound){
+        if (matchFound) {
             return res.json(hashedEmail);
         }
         // If no match found, returns "Email not found"
-        else{
-            return res.json({emailNotFound: "Email not found"}); 
+        else {
+            return res.status(400).json({emailNotFound: "Email not found"});
         }
 
-    });
+
+    }); }
 }
 
 // ROUTER.PARAM MIDDLEWARE
@@ -231,9 +245,12 @@ exports.studentByEmail = (req, res, next, email) => {
             if(matchFound){
                 req.student = foundStudent;
                 next();
+                validateRegisterInput(err);
             }
+            //another for nomatch found that will allow to add new student without
+                //displaying {"studentNotFound":"Student not found"}
             else{
-                return res.json({studentNotFound: "Student not found"}); 
+                //return res.json({studentNotFound: "Student not found"});
             }
         }
     });
--- backend/validation/login.js
+++ backend/validation/login.js
@@ -1,22 +1,24 @@
 const Validator = require("validator"),
-  isEmpty = require("is-empty");
+    isEmpty = require("is-empty");
 
 module.exports = function validateRegisterInput(data) {
-  // Holds any email validation errors
-  let errors = {};
+    // Holds any email validation errors
+    let errors = {};
 
     data.email = !isEmpty(data.email) ? data.email : "";
 
     // Checking if email is entered and valid
-    if(Validator.isEmpty(data.email)){
-      errors.email = "Email field is required";
-    } 
-    else if(!Validator.isEmail(data.email)){
-      errors.email = "Invalid email";
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email Field is Required";
+    }
+    /*
+    Checks if the email is a valid email*/
+    else if (!Validator.isEmail(data.email)) {
+        errors.email = "Invalid email";
     }
 
     return {
-      errors,
-      isValid: isEmpty(errors)
+        errors,
+        isValid: isEmpty(errors)
     };
 };
