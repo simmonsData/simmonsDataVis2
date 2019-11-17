3a8d45e8d49261c52e709966228b54a70fe94ad0
blaisebowman <blaiseabowman@ufl.edu>
2019-11-16 20:19:56 -0500

    Fixed broken App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,39 @@
 import React, { useState } from 'react';
-import {Redirect} from 'react-router-dom';
-import {Grid, Image, Header, Container, Button, Divider, Segment, GridColumn} from 'semantic-ui-react'
-import '../styles/Homepage.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function Homepage(props) {
+//Pages
+import EmailEntry from './pages/EmailEntry'
+import adminEntry from './pages/adminEntry'
+import Homepage from './pages/Homepage'
+import SurveyPage from './pages/SurveyPage';
 
-    const [toData, setToData] = useState(false);
-    const [toSurvey, setToSurvey] = useState(false);
+//Components
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-    function onDataPress() {
-        props.history.push('/data');
-    }
+import './styles/App.css'
 
-    function onSurveyPress() {
-        props.history.push('/survey')
-    }
+function App() {
+
+    const[userID, setUserID] = useState('');
+
+    function userLogged(id) { setUserID(id);};
 
     return (
-        <div className="homePage">
-            <Divider/>
-            <Header as='h1' textAlign='center'>Engineering Out-Of-Class Activity Decision Support System</Header>
-            <Divider/>
-            <Container text>
-                <Segment inverted color="grey">
-                    <p>The OOCA Decision Support System aims at guiding students (i.e. especially
-                        undergraduate engineering) on making decisions on professional development plan
-                        and support educators, program managers and policy makers on developing
-                        strategic plan for engineering programs. This system is developed as part of the
-                        NSF project: CAREER:Investigating Co-Curricular Participation of Students
-                        Underrepresented in Engineering. The full project description can be found on: &nbsp;
-                        <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1351156&HistoricalAwards=false">here
-                        </a>.</p>
-                    <p>This system uses public data and survives by your contributions. Please take your time
-                        to complete data the survey. By doing so, you are helping to improve the system. Please note
-                        that
-                        you need to be at least 18 years old to take this survey.</p>
-                </Segment>
-            </Container>
-            <Grid columns='2'>
-                <Grid.Column>
-                    <Button
-                        onClick={onDataPress}
-                        size='massive'
-                        className='router'>
-                        &nbsp; Data &nbsp;
-                    </Button>
-                </Grid.Column>
-                <Grid.Column>
-                    <Button
-                        onClick={onSurveyPress}
-                        size='massive'
-                        className='router'>
-                        Survey
-                    </Button>
-                </Grid.Column>
-            </Grid>
-            <Divider/>
-            <Divider/>
-            <Divider/>
-        </div>
+        <Router>
+            <div className='container'>
+                <Header/>
+                <main>
+                    <Route exact path="/" render={(props) => <EmailEntry {...props} userLogged={userLogged.bind(this)} />}/>
+                    <Route exact path="/Homepage" component={Homepage} />
+                    <Route exact path="/survey" component={SurveyPage} />
+                    <Route exact path="/data" component={Homepage} />
+                    <Route exact path="/admin" component={adminEntry} />
+                </main>
+                <Footer/>
+            </div>
+        </Router>
     );
 }
 
-export default Homepage;
+export default App;
