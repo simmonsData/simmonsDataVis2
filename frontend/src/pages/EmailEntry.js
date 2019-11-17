00599344dadda0728603ad83a59d4fe892b5d383
blaisebowman <blaiseabowman@ufl.edu>
2019-11-16 20:28:06 -0500

    Updated EmailEntry.js and Homepage.js

--- frontend/src/pages/EmailEntry.js
+++ frontend/src/pages/EmailEntry.js
@@ -1,117 +1,176 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState} from 'react';
 import { Redirect } from 'react-router-dom';
 
-import { Button, Header, Form, Grid, Divider } from 'semantic-ui-react'
+
+import {Button, Header, Form, Grid, Divider, Segment, Label} from 'semantic-ui-react'
 
 import axios from 'axios'
 
 import '../styles/EmailEntry.css'
 
+
 function EmailEntry(props) {
 
   const [errors, setErrors] = useState({});
-
+  const [errorsL, setErrorsL] = useState({});
   const [registerInput, setregisterInput] = useState('')
   const [loginInput, setLoginInput] = useState('')
   const [redirecting, setRedirecting] = useState(false)
 
   function registerChange(e) {
     setregisterInput(e.target.value);
+    if(loginInput != null){
+      setLoginInput('');
+      //removes any text in the login form
+    }
+    if (errors != null){
+      //closes the popup for register
+      setErrors({});
+    }
+    if (errorsL !=null) {
+      //closes the popup for login
+      setErrorsL({});
+    }
   }
 
   function loginChange(e) {
     setLoginInput(e.target.value);
-  }
-  
-  async function onRegister() {
-
-    const response = await axios.post(
-      'http://localhost:8080/api/students/register',
-      { email: registerInput },
-      { headers: { 'Content-Type': 'application/json' } }
-    )
-    if(response.data.emailFound){
-      console.log('email already exists')
-    } else {
-      console.log(response);
-      props.userLogged(registerInput);
-      setRedirecting(true);
+    if(registerInput != null){
+      setregisterInput('');
+      //removes any text in the register form
     }
+    if (errorsL != null){
+      //closes the popup for login
+      setErrorsL({});
+    }
+
+    if (errors !=null) {
+      //closes the popup for register
+      setErrors({});
+    }
+  }
+
+
+  async function onRegister(e) {
+    if(loginInput != null){
+      setLoginInput('');
+    }
+    if (errorsL != null){
+      //closes the popup for login
+      setErrorsL({});
+    }
+    const response = await axios.post(
+        'http://localhost:8080/api/students/register',
+        {email: registerInput},
+        {headers: {'Content-Type': 'application/json'}}
+    )
+        .then(function (response) {
+          console.log(response);
+          props.userLogged(registerInput);
+          setRedirecting(true);
+        })
+        .catch(function (error) {
+          setErrors(error.response.data.email);
+          console.log(errors);
+        });
   }
 
   async function onLogin(e) {
-
+    if(setregisterInput != null){
+      setregisterInput('');
+    }
+    if (errors != null){
+      //closes the popup for register
+      setErrors({});
+    }
     const response = await axios.post(
-      'http://localhost:8080/api/students/login',
-      { email: loginInput },
-      { headers: { 'Content-Type': 'application/json' } }
-    )
-    if(response.data.emailNotFound){
-      console.log('email does not exists')
-    } else {
+        'http://localhost:8080/api/students/login',
+        { email: loginInput },
+        { headers: { 'Content-Type': 'application/json' } }
+    ).then(function (response) {
       console.log(response);
       props.userLogged(loginInput);
       setRedirecting(true);
-    }
+    })
+        .catch(function (error) {
+          console.log(error.response.data.emailNotFound);
+          if(error.response.data.emailNotFound){
+            setErrorsL(error.response.data.emailNotFound);
+          }
+          else {
+            setErrorsL(error.response.data.email);
+          }
+        });
   }
 
   if (redirecting) {
     return <Redirect to='/Homepage' />
   } else {
     return(
-    <div className='EmailEntry'>
-      <Header as='h1' className='welcome'>
-        Welcome to our survey
-      </Header>
-      <Header as='h3' className='welcome'>
-        New users please register with your UFL email and fill out a survey
-      </Header>
-      <Header as='h3' className='welcome'>
-      Returning users can edit their survey and view their results
-      </Header>
-      <Divider/>
-      <Divider/>
-      <Grid columns = {2} divided className='emailForms'>
-        <Grid.Row>
-          <Grid.Column>
-            <Form className='emailInput' onSubmit={onRegister}>
-              <Header as='h5'>
-                I am a new user
-              </Header>
-              <Form.Field className='emailInput'>
-                <label>Email Address</label>
-                <input 
-                  placeholder='Email' 
-                  onChange={registerChange}
-                  value={registerInput}
-                  error={errors.registerInput}
-                />
-              </Form.Field>
-              <Button type='submit'>Submit</Button>
-            </Form>
-          </Grid.Column>
-          <Grid.Column>
-            <Form className='emailInput' onSubmit={onLogin}>
-              <Header as='h5'>
-                I have submitted a survey
-              </Header>
-              <Form.Field className='emailInput'>
-                <label>Email Address</label>
-                <input 
-                  placeholder='Email' 
-                  onChange={loginChange}
-                  value={loginInput}
-                  error={errors.loginInput}
-                />
-              </Form.Field>
-              <Button type='submit'>Submit</Button>
-            </Form>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div>
+        <div className='EmailEntry'>
+          <Header as='h1' className='welcome'>
+            Welcome to our survey
+          </Header>
+          <Header as='h3' className='welcome'>
+            New users please register with your UFL email and fill out a survey
+          </Header>
+          <Header as='h3' className='welcome'>
+            Returning users can edit their survey and view their results
+          </Header>
+          <Divider/>
+          <Divider/>
+          <Grid columns = {2} divided className='emailForms'>
+            <Grid.Row>
+              <Grid.Column>
+                <Form className='emailInput' onSubmit={onRegister}>
+                  <Header as='h5'>
+                    I am a new user
+                  </Header>
+                  <Form.Field className='emailInput'>
+                    <label>Email Address</label>
+                    <input
+                        placeholder='Email'
+                        onChange={registerChange}
+                        onClick = {registerChange}
+                        value={registerInput}
+                        autoComplete="off"
+                    />
+                    {errors.length > 0 &&
+                    <Label pointing color='red' size="tiny"
+                    ><Segment size = "small" inverted color = 'red' textAlign="center">{errors}</Segment></Label>
+                    }
+                  </Form.Field>
+                  <Button type='submit'
+                          onClick = {onRegister}>Submit</Button>
+
+                </Form>
+              </Grid.Column>
+              <Grid.Column>
+                <Form className='emailInput' onSubmit={onLogin}>
+                  <Header as='h5'>
+                    I have submitted a survey
+                  </Header>
+                  <Form.Field className='emailInput'>
+                    <label>Email Address</label>
+                    <input
+                        placeholder='Email'
+                        onChange={loginChange}
+                        onClick = {loginChange}
+                        value={loginInput}
+                    />
+                    {errorsL.length > 0 &&
+                    <Label pointing color='red' size="tiny"
+                    ><Segment size = "small" inverted color = 'red' textAlign="center">{errorsL}</Segment></Label>
+                    }
+                  </Form.Field>
+                  <Button type='submit'
+                          onClick={onLogin}>Submit</Button>
+                </Form>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
     );
   }
 }
-
 export default EmailEntry;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import {Link} from 'react-router-dom';
-import {Grid, Image, Header, Container, Button, Divider, Segment} from 'semantic-ui-react'
-import '../styles/Homepage.css'
-
-function Homepage() {
-    return (<div className="Homepage">
-
-            <Header as='h1' textAlign='center'>Engineering Out-Of-Class Activity Decision Support System</Header>
-
-            <Container text>
-                <Segment inverted color="black">
-=======
 import React, { useState } from 'react';
 import {Redirect} from 'react-router-dom';
 import {Grid, Image, Header, Container, Button, Divider, Segment, GridColumn} from 'semantic-ui-react'
@@ -38,63 +23,32 @@ function Homepage(props) {
             <Divider/>
             <Container text>
                 <Segment inverted color="grey">
->>>>>>> d1cc6b6d4eb6ea16d4bf4472f13ab51d89d73076
                     <p>The OOCA Decision Support System aims at guiding students (i.e. especially
                         undergraduate engineering) on making decisions on professional development plan
                         and support educators, program managers and policy makers on developing
                         strategic plan for engineering programs. This system is developed as part of the
                         NSF project: CAREER:Investigating Co-Curricular Participation of Students
-<<<<<<< HEAD
-                        Underrepresented in Engineering. The full project description can be found on:
-=======
                         Underrepresented in Engineering. The full project description can be found on: &nbsp;
->>>>>>> d1cc6b6d4eb6ea16d4bf4472f13ab51d89d73076
                         <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1351156&HistoricalAwards=false">here
                         </a>.</p>
                     <p>This system uses public data and survives by your contributions. Please take your time
                         to complete data the survey. By doing so, you are helping to improve the system. Please note
                         that
                         you need to be at least 18 years old to take this survey.</p>
-<<<<<<< HEAD
-
-                </Segment>
-            </Container>
-
-            <Router>
-                <Grid relaxed columns='equal'>
-                    <Grid.Row>
-                        <Grid.Column textAlign='center'>
-                            <Link to='/Data'>
-                                <Button size='massive' color='red'>
-                                    Data
-                                </Button>
-                            </Link>
-                        </Grid.Column>
-                        <Grid.Column textAlign='center'>
-                            <Link to='/Survey'>
-                                <Button size='massive' color='red'>
-                                    Survey
-                                </Button>
-                            </Link>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Router>
-=======
                 </Segment>
             </Container>
             <Grid columns='2'>
                 <Grid.Column>
-                    <Button 
-                        onClick={onDataPress} 
+                    <Button
+                        onClick={onDataPress}
                         size='massive'
                         className='router'>
                         &nbsp; Data &nbsp;
                     </Button>
                 </Grid.Column>
                 <Grid.Column>
-                    <Button 
-                        onClick={onSurveyPress} 
+                    <Button
+                        onClick={onSurveyPress}
                         size='massive'
                         className='router'>
                         Survey
@@ -104,7 +58,6 @@ function Homepage(props) {
             <Divider/>
             <Divider/>
             <Divider/>
->>>>>>> d1cc6b6d4eb6ea16d4bf4472f13ab51d89d73076
         </div>
     );
 }
