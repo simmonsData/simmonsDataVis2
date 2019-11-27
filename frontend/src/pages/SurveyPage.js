191d1c1c8d7cce6aaeb4608b650d6374c14e8d2c
blaisebowman <blaiseabowman@ufl.edu>
2019-11-27 14:00:10 -0500

    Updated the Survey page and the Data page with working buttons that re-direct the user to the homepage

--- frontend/src/pages/SurveyPage.js
+++ frontend/src/pages/SurveyPage.js
@@ -1,22 +1,11 @@
 import React, {Component, useState} from 'react';
-import { Redirect } from 'react-router-dom';
-import axios from 'axios';
-
 import * as Survey from 'survey-react';
-import data from '../data/survey.js';
-
 import "survey-react/survey.css";
+import data from '../data/survey.js';
 import '../styles/SurveyPage.css';
 import {Button, Grid, Icon, Modal, Divider} from "semantic-ui-react";
 
 class SurveyPage extends Component {
-    // redirecting handles redirecting to data page
-    constructor(props){
-        super(props);
-        this.state={
-            redirecting: false
-        }
-    }
     json = data;
     constructor(props) {
         super(props);
@@ -28,38 +17,13 @@ class SurveyPage extends Component {
         this.onHomePress = this.onHomePress.bind(this);
     }
 
-
-    // Called when a question in the survey is changed/answered
-    onValueChanged = (result) => {
-
+    onValueChanged(result) {
         console.log("value change!");
     }
 
-    // Called when survey is submitted
-    onComplete = (result) => {
-        // console.log("Complete! "+ JSON.stringify(result.data));
-
-        // user is appended to route to make put request
-        const id = this.props.id;
-        console.log(this.props.id);
-        axios.put(
-            'http://localhost:8080/api/students/' + id,
-            { survey: result.data },
-            { headers: { 'Content-Type': 'application/json' } }
-        )
-        .then(res => {
-            console.log(res);
-
-            // If put request is successful, sets redirecting to true
-            this.setState({
-                redirecting: true
-            })
-        })
-        .catch(err => {
-            console.log(err);
-        })
-
-    };
+    onComplete(result) {
+        console.log("Complete! "+ JSON.stringify(result.data));
+    }
 
     onHomePress(props) {
         this.props.history.push('/Homepage');
@@ -75,13 +39,7 @@ class SurveyPage extends Component {
     }
 
     render() {
-        const model = new Survey.Model(this.json);
-
-        // Redirects to data page when survey is complete
-        if(this.state.redirecting) {
-            return <Redirect to='/data' />
-        }
-
+        var model = new Survey.Model(this.json);
         return (
             <div className="surveyjs">
                 <Survey.Survey
