5b413c370bd4e4c16d014867673126830a5c0357
jlu38 <joeylu38@gmail.com>
2019-11-30 19:54:36 -0500

    comment out config file

--- backend/config/app.js
+++ backend/config/app.js
@@ -1,10 +1,10 @@
-const config = require('./config'), 
-    mongoose = require('mongoose'),   
+const mongoose = require('mongoose'),  
+    // config = require('./config'),  
     express = require('./express');
 
 module.exports.start = function() {
     const app = express.init();
-    const port = process.env.PORT || config.port;
+    const port = process.env.PORT || 8080;
     app.listen(port, () => 
         console.log(`Server now running on port `, port)
     );
