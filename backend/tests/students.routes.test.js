449bbc40a732e3d307a8ae7eb9343e32990a6352
jlu38 <joeylu38@gmail.com>
2019-12-02 21:43:20 -0500

    add api request tests

--- backend/tests/students.routes.test.js
+++ backend/tests/students.routes.test.js
@@ -3,7 +3,7 @@ const should = require('should'),
     express = require('../config/express'), 
     Student = require('../models/students.model.js');
 
-const app, agent, id;
+let app, agent, id;
 
 // 1) Register new email address - should pass
 // 2) Try to register old email address - should fail
@@ -11,7 +11,7 @@ const app, agent, id;
 // 4) Try to login with email in database - should pass
 // 5) Try to login with email not in database - should fail
 // 5) Try to login email in incorrect format - should fail
-describe('Student register and login tests', function(){
+describe('Student register,login, and CRUD tests', function(){
     this.timeout(10000);
 
     before(function(done) {
@@ -25,15 +25,12 @@ describe('Student register and login tests', function(){
         const student = {
             email: 'test1@test.com',
         };
-        agent.post('/api/students')
+        agent.post('/api/students/register')
             .send(student)
             .expect(200)
             .end(function(err, res) {
-                should.not.exist(err);
-                should.exist(res.body._id);
-                should.exist(res.body.email);
                 should.exist(res);
-                id = res.body._id;
+                id = res.body
                 done();
             });
     })
@@ -41,12 +38,106 @@ describe('Student register and login tests', function(){
         const student = {
             email: 'test1@test.com',
         };
-        agent.post('/api/students')
+        agent.post('/api/students/register')
             .send(student)
-            .expect(404)
+            .expect(400)
             .end(function(err, res) {
-                should.exist(err);
+                should.not.exist(err);
+                should.exist(res);
+                res.body.emailFound.should.equal('Email already created');
                 done();
             });
+    })
+    it('should not register email address in incorrect format', function(done){
+        const student = {
+            email: 'test1@',
+        };
+        agent.post('/api/students/register')
+            .send(student)
+            .expect(400)
+            .end(function(err, res) {
+                should.not.exist(err);
+                should.exist(res);
+                done();
+            });
+    })
+    it('logins email address to database', function(done){
+        const student = {
+            email: 'test1@test.com',
+        };
+        agent.post('/api/students/login')
+            .send(student)
+            .expect(200)
+            .end(function(err, res) {
+                should.not.exist(err);
+                should.exist(res);
+                id = res.body;
+                done();
+            });
+    })
+    it('should not login email address not in database', function(done){
+        const student = {
+            email: 'fagre@test.com',
+        };
+        agent.post('/api/students/login')
+            .send(student)
+            .expect(400)
+            .end(function(err, res) {
+                should.not.exist(err);
+                should.exist(res);
+                res.body.emailNotFound.should.equal('Email not found');
+                done();
+            });
+    })
+    it('should not login email address already in incorrect format', function(done){
+        const student = {
+            email: 'test1@',
+        };
+        agent.post('/api/students/login')
+            .send(student)
+            .expect(400)
+            .end(function(err, res) {
+                should.not.exist(err);
+                should.exist(res);
+                done();
+            });
+    })
+    it('should retrieve all students', function(done){
+        agent.get('/api/students')
+            .expect(200)
+            .end(function(err, res){
+                should.not.exist(err);
+                should.exist(res);
+                done();
+            })
+    })
+    it('should be able to retrieve a single student', function(done){
+        Student.findOne({email: 'test1@test.com'}, function(err, student){
+            if(err){
+                console.log(err);
+            }
+            else{
+                agent.get('/api/students/' + student._id)
+                    .expect(200)
+                    .end(function(err, res) {
+                        should.not.exist(err);
+                        should.exist(res);
+                        res.body.email.should.equal('test1@test.com');
+                        done();
+                    })
+            }
+        })
+    })
+
+    after(function(done) {
+        console.log(id);
+        if(id) {
+            Student.deleteOne({ _id: id }).exec(function() {
+              id = null;
+              done();
+            });
+          } else {
+            done();
+          }
     })
 })
