c6141563950836ae1851a941d3803fcb4dfa6246
Ivan Zundel <ivan.zundel@ufl.edu>
2019-12-03 01:18:03 -0500

    Update adminPanel.js

--- frontend/src/pages/adminPanel.js
+++ frontend/src/pages/adminPanel.js
@@ -31,7 +31,7 @@ class adminPanel extends Component {
           height: "85vh",
         }}
       >
-        <Grid>//added two buttons
+        <Grid>
           <Grid.Column textAlign="center">
             <Button primary size="massive" onClick={this.onSurveyPress}>
               {" "}
