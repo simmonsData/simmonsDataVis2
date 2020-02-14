e9098f8d1bccca52882cda6f2ebed4991b2c01c4
jlu38 <joeylu38@gmail.com>
2020-02-14 15:02:44 -0500

    change header and homepage colors and styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-import {Button, Grid, Divider, Image, Menu, Icon} from 'semantic-ui-react'
+import {Button, Grid, Image, Menu} from 'semantic-ui-react'
 
 import '../styles/App.css'
 import '../styles/Header.css'
@@ -10,20 +10,24 @@ import Logo from '../img/simmons-bktagline-2.png';
 function Header () {
   return (
     <header>
-      <Grid id="headerMenu" columns={3}>
+      <Grid id="headerMenu" columns={3} verticalAlign="middle">
         <Grid.Row>
-          <Grid.Column>
+          <Grid.Column id="leftColumn">
             <Image id="logo" src={Logo}></Image>
+            <Button 
+              color='#a32638' 
+              className="menuText" 
+              textAlign="left" 
+              as={Link} 
+              to='/' 
+              active>
+                Home
+            </Button>
+            <Button className="menuText" as={Link} to='/'>About Us</Button>
          </Grid.Column>
-          <Grid.Column>
-            <Button color='#a32638' className="menuText" textAlign="left" as={Link} to='/' active>Home</Button>
-            <Button className="menuText" as={Link} to='/' active>About Us</Button>
-          </Grid.Column>
 
-          {/* <Grid.Column /> */}
-
-          <Grid.Column id="rightColumn">
-            <Button className="menuText" as={Link} to='/login'>Login/Register</Button>
+          <Grid.Column id="rightColumn" floated="right">
+            <Button className="menuText" as={Link} to='/login'><span>Login/Register</span></Button>
             <Menu.Item as={Link} to='/survey'>
               <Button id="surveyButtonMenu" >Take Survey</Button>
             </Menu.Item>
