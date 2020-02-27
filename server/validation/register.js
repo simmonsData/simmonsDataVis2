30a97d9288981cc083ac5cab7329827ad4028b1e
jlu38 <joeylu38@gmail.com>
2020-02-26 22:15:27 -0500

    add password validation for register

--- server/validation/register.js
+++ server/validation/register.js
@@ -7,6 +7,8 @@ module.exports = function validateRegisterInput(data) {
     let errors = {};
 
     data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+
 
     // Checking if email is entered and valid
     if (Validator.isEmpty(data.email)) {
@@ -17,7 +19,16 @@ module.exports = function validateRegisterInput(data) {
     else if (!Validator.isEmail(data.email)) {
         errors.email = "Error: Invalid Email Format";
     }
-//Pre-existing emails are already checked in EmailEntry.js
+    
+    // Checks if the password is entered and must be between 6-30 characters
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
+    }
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+        errors.password = "Password must be at least 6 characters";
+    }
+    
+    //Pre-existing emails are already checked in EmailEntry.js
 
     return {
         errors,
