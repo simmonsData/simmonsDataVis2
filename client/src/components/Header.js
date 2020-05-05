e5278e87b0c337cb286a7daf43ed07a18a992493
jlu38 <joeylu38@gmail.com>
2020-05-05 17:55:14 -0400

    header maintains id if logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import {Button,Grid, Image, Menu} from 'semantic-ui-react'
 
 import '../styles/App.css'
@@ -8,27 +8,27 @@ import '../styles/Header.css'
 import Logo from '../img/simmons-bktagline-2.png';
 
 function Header () {
+  const history = useHistory();
+
+  const onButtonPress = (event) => {
+    let id = "";
+    if(sessionStorage.getItem("loggedIn"))
+      id = sessionStorage.getItem("id");
+    history.push(event.target.value + id);
+  }
 
   return (
     <header>
       <Grid id="headerMenu" columns={3} verticalAlign="middle">
         <Grid.Row>
           <Grid.Column width="3">
-              <Image id="logo" src={Logo} as={Link} to='/'></Image>
+              <Image id="logo" src={Logo} as={Link} to="/"></Image>
           </Grid.Column>
 
           <Grid.Column width="5">
-            <Button 
-              className="menuText" 
-              name="home"
-              as={Link} 
-              to='/' 
-              active
-              >
-                Home
-            </Button>
-            <a className="menuText"  href='https://faculty.eng.ufl.edu/simmons-research-lab/about/'>About Us</a> 
-            <Button className="menuText" name="dashboard" as={Link} to='/dashboard/'>Dashboard</Button>
+            <Button className="menuText" name="home" value="/" onClick={onButtonPress} active>Home</Button>
+            <a className="menuText"  href="https://faculty.eng.ufl.edu/simmons-research-lab/about/">About Us</a> 
+            <Button className="menuText" name="dashboard" value="/dashboard/" onClick={onButtonPress}>Dashboard</Button>
           </Grid.Column>
           {/* <Grid.Column width="2">
             <a className="menuText"  href='https://faculty.eng.ufl.edu/simmons-research-lab/about/'>About Us</a> 
@@ -41,8 +41,8 @@ function Header () {
 
           <Grid.Column id="rightColumn" width="3">
             <Button className="menuText loginReg" as={Link} to='/access'>Login/Register</Button>
-            <Menu.Item as={Link} to='/survey/'>
-              <Button id="surveyButtonMenu">Take Survey</Button>
+            <Menu.Item onClick={onButtonPress}>
+              <Button id="surveyButtonMenu" value="/survey/" onClick={onButtonPress}>Take Survey</Button>
             </Menu.Item>
           </Grid.Column>
         </Grid.Row>
